fd598fb881dfe39313cd004d3590197ce70b9862
Ashraf Ahmed <ahmashraf@gmail.com>
2023-09-14 18:04:05 +0600

    Minor Change in Personal Information File

--- personal_info.php
+++ personal_info.php
@@ -30,7 +30,7 @@
                         $profession = "teacher";
                         $briefIntro = "I am from Moulvibazar Sadar, Moulvibazar. I live with my parents. I am fond of online contents. I write blogs using WordPress CMS and also edit videos. I use online contents to a great extent to connect with my students.";
 
-                        echo "My name is {$name} and I am {$age} at the moment. I am from {$country} and I love my country a lot. I am a {$profession} at the moment but I fell in love with codes years ago.";
+                        echo "My name is {$name} and I am {$age} at the moment. I am from {$country} and I love my country a lot. I am a {$profession} now but I fell in love with codes years ago.";
                         echo PHP_EOL;
                         echo "\n";
                         echo "$briefIntro";
